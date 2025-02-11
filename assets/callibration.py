7b59a938a09f7856702c7e4e3d93a09cf3237cef
Tom-Sloan <tsloan81@gmail.com>
2025-02-11 15:31:06 -0500

    Disable simulator service and remove output.txt file
    - Calibrate DJI mini 3
    - Comment out simulator service in docker-compose.yml
    - Remove output.txt file containing directory structure
    - Update NGINX configuration with extended proxy timeout settings
    - Update submodules for reconstruction, server, SLAM, and website

---
+++ assets/callibration.py
@@ -0,0 +1,94 @@
+import numpy as np
+import cv2 as cv
+import glob
+import os
+import pickle
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+ 
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(8,5,0)
+objp = np.zeros((9*6,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+ 
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+#  ./calibration_images/*.jpg
+images = glob.glob('./calibration_images/*.jpg')
+ 
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+ 
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (9,6), None)
+ 
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        print(f"Processing image: {fname}")
+        objpoints.append(objp)
+ 
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+ 
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (9,6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(1)  # Wait for any key press
+    else:
+        print(f"Failed to find chessboard corners in image: {fname}")
+        # delete the image
+        os.remove(fname)
+
+cv.destroyAllWindows()
+print(f"objpoints: {objpoints}")
+print(f"imgpoints: {imgpoints}")
+ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+
+#  Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+pickle.dump((cameraMatrix, dist), open( "calibration.pkl", "wb" ))
+pickle.dump(cameraMatrix, open( "cameraMatrix.pkl", "wb" ))
+pickle.dump(dist, open( "dist.pkl", "wb" ))
+
+
+############## UNDISTORTION #####################################################
+
+img = cv.imread('calibration_images/1739236816709505024.jpg')
+h,  w = img.shape[:2]
+newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
+
+
+
+# Undistort
+dst = cv.undistort(img, cameraMatrix, dist, None, newCameraMatrix)
+
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('caliResult1.png', dst)
+
+
+
+# Undistort with Remapping
+mapx, mapy = cv.initUndistortRectifyMap(cameraMatrix, dist, None, newCameraMatrix, (w,h), 5)
+dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('caliResult2.png', dst)
+
+
+
+
+# Reprojection Error
+mean_error = 0
+
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
+
+print( "total error: {}".format(mean_error/len(objpoints)) )
